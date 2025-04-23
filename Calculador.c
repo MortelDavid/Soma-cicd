117fe3c0d14ce9db249feaa6cd8839f390f98818
MortelDavid <morteldavi@gmail.com>
2025-04-22 22:17:25 -0300

    Create Calculador.c

---
+++ Calculador.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int num1, num2, soma;
+
+  printf("digite o primeiro numero:");
+  scanf("%d" , &num1);
+
+  printf("digite o primeiro numero:");
+  scanf("%d" , &num2);
+
+  soma = num1 + num2;
+
+  printf(" A soma de %d e %d é: %d\n",num1, num2, soma);
+  return 0
+}
